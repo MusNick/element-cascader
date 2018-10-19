a774ceafc8601fca7a27cc2b2b65b33161f02703
Nick <cs1127511@gmial.com>
2018-10-19 23:53:54 +0800

    记录element级联前端数据

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,33 @@
+
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>前端地址级联</h1>
+    <el-cascader
+      placeholder="试试搜索：指南"
+      v-model="selectAddress"
+      :options="options"
+      filterable
+    ></el-cascader>
   </div>
 </template>
 
+
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import address from '../static/area/address.json'
 
 export default {
   name: 'app',
   components: {
     HelloWorld
+  },
+  data () {
+    return {
+      options: address,
+      selectAddress: []
+    }
+  },
+  methods: {
   }
 }
 </script>
